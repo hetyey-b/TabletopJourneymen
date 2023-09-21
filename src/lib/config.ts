752596a36dcf57f3de37ddbeeaa50f6706bed4d0
hetyey-b <hetyey.botond@gmail.com>
2023-09-21 07:52:32 +0200

    initial setup

---
+++ src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment'
+
+export const title = 'Tabletop Journeymen'
+export const description = 'A blog about tabletop RPG and boardgame design'
+export const url = dev ? 'http://localhost:5173/' : 'TODO'
---
+++ src/routes/header.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+    import * as config from '$lib/config'
+</script>
+
+<nav class="px-2.5 py-1">
+    <a href="/" class="">
+        <b>{config.title}</b>
+    </a>
+
+    <ul>
+        <li>
+            <a href="/contact">Contact</a>
+        </li>
+        <li>
+            <a href="/rss.xml" target="_blank">RSS</a>
+        </li>
+
+        <button>Toggle dark mode</button>
+    </ul>
+</nav>
